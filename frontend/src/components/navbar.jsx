a19c27f8374caa1a2231cfb9bbdb25471aa7ef6d
frankie siino <frankie.siino@gmail.com>
2019-09-19 13:38:59 -0700

    Title, price, time and date created on product index

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
+import './navbar.scss';
+
 class NavBar extends React.Component {
 
   render() {
     return (
-      <div>
-        This is the navbar/header
+      <div className="navbar-container">
+        <div className="nb-logo-wrapper">
+          <div>HP Store</div>
+          
+          <img src="https://ssl.www8.hp.com/us/en/images/i/hpi/header-footer/caas-hf-v3.2/hpi-hp-logo-pr.gif" className="hp-logo"/>
+        </div>
+
+        <div className="welcome">
+          Welcome, user
+        </div>
+
+        <div className="nav-links">
+          Sign In
+        </div>
       </div>
     )
   }
--- frontend/src/components/products/products.jsx
+++ frontend/src/components/products/products.jsx
@@ -6,7 +6,8 @@ class Products extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: props.products
+      products: props.products,
+      sorted: false
     };
   }
   
@@ -18,6 +19,31 @@ class Products extends React.Component {
 
   render() {
 
+    // const intToFloat = (num, decPlaces) => { return num.toFixed(decPlaces); };
+
+    const convertDate = (date) => {
+      let newDate = new Date(date);
+      return `${1 + newDate.getMonth()}/${newDate.getDate()}/${1900 + newDate.getYear()}`;
+    };
+
+    const convertTime = (time) => {
+      let newDate = new Date(time);
+      let amPm;
+      let hours = newDate.getHours();
+      let mins = newDate.getMinutes();
+
+      if (hours > 12) {
+        amPm = 'PM';
+        hours -= 12;
+      } else {
+        amPm = 'AM';
+      }
+
+      mins < 10 ? mins = `0${mins}` : mins = mins;
+      
+      return `${hours}:${mins} ${amPm}`;
+    };
+
     if (!this.props.products) return 'loading';
 
     return (
@@ -26,6 +52,10 @@ class Products extends React.Component {
           <div className="product-idx-tile" key={product.product_id}>
             <h3>{product.title}</h3>
               <img src={product.media[0].sizes[Array.length-1].url} className="product-idx-img"/>
+              <div>
+                <h5>${product.price}.00</h5>
+                <h5>Date Listed: {convertDate(product.created_at)}, at {convertTime(product.created_at)}</h5>
+              </div>
           </div>
         )}
       </div>
