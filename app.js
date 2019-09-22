60510f9cf0203cd4f02c22051a94dadbc6adca79
frankie siino <frankie.siino@gmail.com>
2019-09-21 21:18:53 -0700

    Load static build folder in prod

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const products = require("./routes/api/products");
@@ -27,3 +28,10 @@ app.use("/api/products", products);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
