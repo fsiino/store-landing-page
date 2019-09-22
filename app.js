ab4bfdfa2a8a9e228aa4a0e7237181286366f9a1
frankie siino <frankie.siino@gmail.com>
2019-09-21 22:35:39 -0700

    Change order of ops in appjs

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 app.get("/", (req, res) => res.send("Hello World!!"));
 
 app.use(passport.initialize());
@@ -30,9 +37,3 @@ app.use("/api/products", products);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
