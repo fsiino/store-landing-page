1fc8dcd9fb603e57646365eab5465c83d3a76dc1
frankie siino <frankie.siino@gmail.com>
2019-09-24 14:52:06 -0700

    Add demo user functionality to login-form

--- frontend/src/components/session/login_form.jsx
+++ frontend/src/components/session/login_form.jsx
@@ -14,6 +14,8 @@ class LoginForm extends React.Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.renderErrors = this.renderErrors.bind(this);
+    // this.handleDemoUser = this.handleDemoUser.bind(this);
+    this.animateDemoUser = this.animateDemoUser.bind(this);
   }
 
   // Once the user has been authenticated, redirect to the products page
@@ -33,15 +35,51 @@ class LoginForm extends React.Component {
     });
   }
 
+  async animateDemoUser(e) {
+    e.preventDefault();
+
+    const demoUser = {
+      email: 'demo@email.com',
+      password: 'password'
+    };
+
+    const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+    document.getElementById('email-input').focus();
+    for (let i = 1; i <= demoUser.email.length; i++) {
+      this.setState({ email: demoUser.email.substr(0, i) });
+      await sleep(50);
+    }
+
+    await sleep(250);
+
+    document.getElementById('password-input').focus();
+    for (let i = 1; i <= demoUser.password.length; i++) {
+      this.setState({ password: demoUser.password.substr(0, i) });
+      await sleep(50);
+    }
+
+    await sleep(250);
+
+    document.getElementById('session-submit-btn').click();
+    document.getElementById('password-input').blur();
+  }
+
+  handleDemoUser(e) {
+    e.preventDefault();
+    this.props.login({
+      email: 'demo@email.com',
+      password: 'password'
+    });
+  }
+
   // Handle form submission
   handleSubmit(e) {
     e.preventDefault();
-
     let user = {
       email: this.state.email,
       password: this.state.password
     };
-
     this.props.login(user);
   }
 
@@ -71,17 +109,21 @@ class LoginForm extends React.Component {
             <br/>
             <input type="text"
               value={this.state.email}
-              onChange={this.update('email')}
+              onChange={this.update('email')} 
+              id="email-input"
             />
             <br />
             <label>Password:</label>
             <br/>
             <input type="password"
               value={this.state.password}
-              onChange={this.update('password')}
+              onChange={this.update('password')} 
+              id="password-input"
             />
             <br />
-            <input type="submit" value="Submit" />
+            <input id="session-submit-btn" type="submit" value="Submit" />
+            <br/>
+            <input type="submit" onClick={this.animateDemoUser} value="Demo Login" />
             {this.renderErrors()}
           </div>
         </form>
